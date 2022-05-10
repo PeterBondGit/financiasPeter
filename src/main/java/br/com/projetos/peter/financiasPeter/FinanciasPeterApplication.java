e660082b7ed84702620a040f37223508a4063c8c
Peter Bond <peterebond@gmail.com>
2022-05-10 08:46:47 -0300

    Primeiro Commit Projeto Peter

---
+++ src/main/java/br/com/projetos/peter/financiasPeter/FinanciasPeterApplication.java
@@ -0,0 +1,14 @@
+package br.com.projetos.peter.financiasPeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanciasPeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanciasPeterApplication.class, args);
+		System.out.println("Começando Projeto");
+	}
+
+}
---
+++ src/test/java/br/com/projetos/peter/financiasPeter/FinanciasPeterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetos.peter.financiasPeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanciasPeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
